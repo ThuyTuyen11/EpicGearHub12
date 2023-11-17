5da8aff30e4709166fba37997e0cf4276ffa1f6b
ThuyTuyen11 <thuytuyenmainguyen@gmail.com>
2023-11-17 16:19:14 +0700

    demo

---
+++ src/main/java/com/store/demo/service/ProductService.java
@@ -0,0 +1,22 @@
+package com.store.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.store.demo.entity.Product;
+
+public interface ProductService {
+
+	List<Product> findAll();
+
+	Product findById(Integer id);
+
+	List<Product> findByCategoryId(String cid);
+
+	void create(Product product);
+
+	void update(Product product);
+
+
+
+}
